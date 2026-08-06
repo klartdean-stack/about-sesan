991b3c89563a197d4b08447a8bef908ecdba1638
klartdean-stack <klartdean@gmail.com>
2026-08-06 22:54:22 +0700

    Add Microsoft Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://about.sesanshop.com"),
@@ -112,6 +113,15 @@ export default function RootLayout({
   />
 
   {children}
+  <Script id="microsoft-clarity" strategy="afterInteractive">
+  {`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "xy7h8hhedo");
+  `}
+</Script>
 </body>
 
       <GoogleAnalytics gaId="G-V4YYJQV2RG" />
